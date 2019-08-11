e219feb2f47b7a388b031d6d87e3a43a4b194df2
Kristina <_midniht_@mail.ru>
2019-08-11 10:33:39 +0300

    Добавляет верстку страницы index (mobile, tablet)

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
-
+navToggle.innerText = ''
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
